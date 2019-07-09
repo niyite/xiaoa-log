43a3b5d5d6d7931e96deb88ea7c9a9a61379b7b5
towanm@foxmail.com <wan0208626645men>
2019-07-09 16:31:57 +0800

    用占位符记录日志

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # xiaoa-log
 jar工程和web工程,记录日志会用到的jar包和log.properties文件
 
+# 日志级别
+trace<debug<info<warn<error<fatal  
+
 # 友情援助  
 (您的支持,是我前进的动力,一分也是爱,有任何疑问请联系QQ:747431644)  
 ![image](https://github.com/niyite/image/blob/master/1552375153915.jpg)
+
+
--- src/com/xiaoa/Log_Demo.java
+++ src/com/xiaoa/Log_Demo.java
@@ -1,30 +1,33 @@
 package com.xiaoa;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @ClassName Log_Demo
- * @Description TODO
+ * @Description 测试日志工具类
  * @Author wanmeng
  * @Date 2019/3/12 13:06
  * @Version 1.0
  **/
 public class Log_Demo {
 
-    private static Logger logger = Logger.getLogger(Log_Demo.class);
+    //日志记录工具类
+    private static Logger log = LoggerFactory.getLogger(Log_Demo.class);
 
     /**
      * @param args
      */
     public static void main(String[] args) {
 
-        // System.out.println("This is println message.");
-
         // 记录debug级别的信息
-        logger.debug("This is debug message.");
+        log.debug("This is debug message.");
         // 记录info级别的信息
-        logger.info("This is info message.");
+        log.info("This is info message.");
         // 记录error级别的信息
-        logger.error("This is error message.");
+        log.error("This is error message.");
+        // 用占位符记录日志
+        log.debug("this is {} message", "debug");
+
     }
 }
